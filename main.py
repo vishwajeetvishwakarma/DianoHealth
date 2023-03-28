900cd8e5bede3b49737e5a0a61c7beb41ff05f1b
vishwajeetvishwakarma <vishwajeetv2003@gmail.com>
2023-03-28 22:46:33 +0530

    we have build second model.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.wsgi import WSGIMiddleware
 from fastapi.templating import Jinja2Templates
 import uvicorn
+from dataclasses import dataclass
 from contextlib import asynccontextmanager
 from typing import Annotated
 import joblib
@@ -20,9 +21,16 @@ ml_models = {}
 async def startup_event():
     scaler_for_diabetes = joblib.load('./data/diabetes_standard_scaler_joblib')
     diabetes_model = joblib.load('./data/diabetes_joblib')
+    lung_cancer = joblib.load("./data/lung_cancer_main_joblib")
     ml_models['diabetes_model'] = diabetes_model
     ml_models['scaler_for_diabetes'] = scaler_for_diabetes
+    ml_models['lung_cancer'] = lung_cancer
     print("Models is Loading")
+
+
+@app.get("/items/")
+async def end_event():
+    return ml_models.clear()
 
 
 @ app.get("/", response_class=HTMLResponse)
@@ -54,6 +62,45 @@ async def get_diabetes(request: Request,
     print(f"So the result is : {result}")
     diabetes = ['No', 'Yes']
     return templates.TemplateResponse("diabetes.html", {'request': request, 'result': diabetes[result[0]]})
+
+
+@ app.get("/lungcancer", response_class=HTMLResponse)
+async def get_lung_cancer(request: Request):
+    return templates.TemplateResponse("lung-cancer.html", {'request': request})
+
+
+@dataclass
+class LungCancerForm:
+    GENDER: Annotated[int, Form()]
+    AGE: Annotated[int, Form()]
+    SMOKING: Annotated[int, Form()]
+    YELLOW_FINGERS: Annotated[int, Form()]
+    ANXIETY: Annotated[int, Form()]
+    PEER_PRESSURE: Annotated[int, Form()]
+    CHRONIC_DISEASE: Annotated[int, Form()]
+    FATIGUE: Annotated[int, Form()]
+    ALLERGY: Annotated[int, Form()]
+    WHEEZING: Annotated[int, Form()]
+    ALCOHOL_CONSUMING: Annotated[int, Form()]
+    COUGHING: Annotated[int, Form()]
+    SHORTNESS_OF_BREATH: Annotated[int, Form()]
+    SWALLOWING_DIFFICULTY: Annotated[int, Form()]
+    CHEST_PAIN: Annotated[int, Form()]
+
+
+@ app.post("/lungcancer", response_class=HTMLResponse)
+async def get_lung_cancer(request: Request, GENDER: Annotated[int, Form()], AGE: Annotated[int, Form()], SMOKING: Annotated[int, Form()],
+                          YELLOW_FINGERS: Annotated[int, Form()], ANXIETY: Annotated[int, Form()], PEER_PRESSURE: Annotated[int, Form()],
+                          CHRONIC_DISEASE: Annotated[int, Form()], FATIGUE: Annotated[int, Form()], ALLERGY: Annotated[int, Form()],
+                          WHEEZING: Annotated[int, Form()], ALCOHOL_CONSUMING: Annotated[int, Form()], COUGHING: Annotated[int, Form()],
+                          SHORTNESS_OF_BREATH: Annotated[int, Form()], SWALLOWING_DIFFICULTY: Annotated[int, Form()], CHEST_PAIN: Annotated[int, Form()],
+                          ):
+    nums = [[GENDER, AGE, SMOKING, YELLOW_FINGERS, ANXIETY, PEER_PRESSURE, CHRONIC_DISEASE, FATIGUE, ALLERGY,
+             WHEEZING, ALCOHOL_CONSUMING, COUGHING, SHORTNESS_OF_BREATH, SWALLOWING_DIFFICULTY, CHEST_PAIN]]
+    result = ml_models["lung_cancer"].predict(nums)
+    print(f"So the result is : {result}")
+    diabetes = ['No', 'Yes']
+    return templates.TemplateResponse("lung-cancer.html", {'request': request, "result": diabetes[result[0]]})
 
 
 if __name__ == "__main__":
